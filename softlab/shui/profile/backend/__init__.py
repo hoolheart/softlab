64fb647536486e822cf07d3f82597116d34e23a0
Edward Chou <prettyage.new@gmail.com>
2023-06-29 09:19:04 +0800

    add profile backend __init__

---
+++ softlab/shui/profile/backend/__init__.py
@@ -0,0 +1,10 @@
+"""
+Profile backend module
+"""
+from softlab.shui.profile.backend.base import ProfileBackend
+from softlab.shui.profile.backend.json_profile import JsonProfileBackend
+
+from softlab.shui.profile.backend.getter import (
+    get_profile_backend,
+    get_profile_backend_by_info,
+)
